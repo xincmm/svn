2e361f433646f166aeb0691e6e399fbc8487d98e
xincmm <licmucm@gmail.com>
2024-12-10 16:19:41 +0800

    feat:新增shadcn-ui

--- pages/content-ui/src/App.tsx
+++ pages/content-ui/src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { Button } from '@extension/ui';
+import { Button } from '@/components/ui/button';
 import { useStorage } from '@extension/shared';
 import { exampleThemeStorage } from '@extension/storage';
 
@@ -10,14 +10,14 @@ export default function App() {
     console.log('content ui loaded');
   }, []);
 
+  console.log('theme', theme);
+
   return (
     <div className="flex items-center justify-between gap-2 rounded bg-blue-100 px-2 py-1">
       <div className="flex gap-1 text-blue-500">
         Edit <strong className="text-blue-700">pages/content-ui/src/app.tsx</strong> and save to reload.
       </div>
-      <Button theme={theme} onClick={exampleThemeStorage.toggle}>
-        Toggle Theme
-      </Button>
+      <Button onClick={exampleThemeStorage.toggle}>Toggle Theme</Button>
     </div>
   );
 }
